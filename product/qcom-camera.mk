3b8f3a1d3824b5175389db8a498ad206c33ddb50
Tet35 <ttnani555@gmail.com>
2019-02-23 14:15:57 +0200

    Move old camera

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,30 +1,18 @@
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8916 \
-    libmm-qcamera \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl \
     Snap
-
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
-
-# Camera
-#PRODUCT_PACKAGES += \
-#    android.hardware.camera.provider@2.4-impl-legacy \
-#    camera.device@1.0-impl \
-#    Snap
 
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/vendor/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/vendor/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
-    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/vendor/etc/permissions/android.hardware.camera.front.xml
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false \
-    persist.camera.hal.debug.mask=0 # \
+    persist.camera.hal.debug.mask=0 \
     camera2.portability.force_api=1
 
 # Make the external camera provider ignore internal cameras
